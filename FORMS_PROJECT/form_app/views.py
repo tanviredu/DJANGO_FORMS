07b511b4ae090191673acf68448abd56f9c0cfee
tanviredu <tanviredu2018@gmail.com>
2020-07-11 21:52:09 +0600

    added

--- FORMS_PROJECT/form_app/views.py
+++ FORMS_PROJECT/form_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import PizzaForm
 
-# Create your views here.
+def home(request):
+    return render(request,'form_app/home.html')
+
+def order(request):
+
+    # this is the manual form
+    form = PizzaForm()
+    return render(request,'form_app/order.html',{'pizzaform':form})
+
+
+
+    
