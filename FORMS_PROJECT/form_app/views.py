5ff7156e93fd15a4b1ff1dda1ca2a0f06e21eecc
tanviredu <tanviredu2018@gmail.com>
2020-07-11 22:12:36 +0600

    added

--- FORMS_PROJECT/form_app/views.py
+++ FORMS_PROJECT/form_app/views.py
@@ -5,11 +5,25 @@ def home(request):
     return render(request,'form_app/home.html')
 
 def order(request):
+    if request.method == "POST":
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            size     = filled_form.cleaned_data['size']
+            topping1 = filled_form.cleaned_data['topping1']
+            topping2 = filled_form.cleaned_data['topping2']
+            print([size,topping1,topping2])
 
-    # this is the manual form
-    form = PizzaForm()
-    return render(request,'form_app/order.html',{'pizzaform':form})
+            note = "Thanks for ordering! your pizza with size {} and {} with {} are on its way !".format(size, topping1, topping2)
+        else:
+            note = "Order is not Created"
 
+        ## after the posting
+        new_form = PizzaForm()
+        return render(request,'form_app/order.html',{'pizzaform':new_form,'note':note})
+    else:
+        new_form = PizzaForm()
+        return render(request,'form_app/order.html',{'pizzaform':new_form})
 
+    
 
     
